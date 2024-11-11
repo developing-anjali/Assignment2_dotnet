8a97dcb220527e7abe44755e6b978f76d7184a18
developing-anjali <116741644+developing-anjali@users.noreply.github.com>
2024-11-11 23:38:49 +0530

    Add files via upload

---
+++ assignemt2/assignemt2/Ques1.cs
@@ -0,0 +1,106 @@
+﻿/*class Student
+{
+    string Name;
+    int RollNo;
+    int[] Marks = new int[5];
+    int TotalMarks;
+    float Percentage;
+
+    static string UniversityName;
+
+    public Student()
+    {
+        Name = "";
+        RollNo = 0;
+        TotalMarks = 0;
+        Percentage = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            Marks[i] = 0;
+        }
+    }
+
+    public Student(string name, int rollNo, int[] marks)
+    {
+        Name = name;
+        RollNo = rollNo;
+        TotalMarks = 0;
+        Percentage = 0;
+        Marks = marks;
+    }
+
+    static Student()
+    {
+        UniversityName = "GLA University";
+    }
+
+    public void ReadData()
+    {
+        Console.Write("Enter Name: ");
+        Name = Console.ReadLine();
+
+        Console.Write("Enter Roll No: ");
+        RollNo = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter marks for 5 subjects:");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Subject " + i + 1 + ":");
+            Marks[i] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+
+    public void CalculateResult()
+    {
+        TotalMarks = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            TotalMarks += Marks[i];
+        }
+        Percentage = TotalMarks / 5;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("University: " + UniversityName);
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Roll No: " + RollNo);
+        Console.Write("Marks: ");
+        foreach (var mark in Marks)
+        {
+            Console.Write(mark + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine("Total Marks: " + TotalMarks);
+        Console.WriteLine("Percentage: " + Percentage + "%");
+    }
+
+    public static void SetName(string universityName)
+    {
+        UniversityName = universityName;
+    }
+
+    public static string GetName()
+    {
+        return UniversityName;
+    }
+}
+
+class Ques1
+{
+    static void Main()
+    {
+        Student.SetName("GLA University");
+
+        Student student1 = new Student();
+        student1.ReadData();
+        student1.CalculateResult();
+        student1.Display();
+
+        int[] marks = { 85, 90, 78, 92, 88 };
+        Student student2 = new Student("Sanskriti", 45, marks);
+        student2.CalculateResult();
+        student2.Display();
+    }
+}
+*/
---
+++ assignemt2/assignemt2/Ques2.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+interface Account
+{
+    void Open_Account();
+    void Close_Account();
+}
+
+interface Customer
+{
+    void Display_Customer_Detail();
+}
+
+class SavingAccount : Account, Customer
+{
+     string CustomerName;
+     int AccountNumber;
+     double Balance;
+
+    public static string BankName;
+
+
+    public SavingAccount()
+    {
+        CustomerName = "";
+        AccountNumber = 0;
+        Balance = 0;
+    }
+
+    public SavingAccount(string customerName, int accountNumber, double initialDeposit)
+    {
+        CustomerName = customerName;
+        AccountNumber = accountNumber;
+        Balance = initialDeposit;
+    }
+
+    static SavingAccount()
+    {
+        BankName = "Pnb Bank";  
+    }
+
+    public void Open_Account()
+    {
+        Console.WriteLine("Account opened for {0} with Account Number: {1}", CustomerName, AccountNumber);
+    }
+
+    public void Close_Account()
+    {
+        Console.WriteLine("Account with Account Number {0} is now closed.", AccountNumber);
+        CustomerName = "";
+        AccountNumber = 0;
+        Balance = 0;
+    }
+
+    public void Display_Customer_Detail()
+    {
+        Console.WriteLine("Customer Name: {0}", CustomerName);
+        Console.WriteLine("Account Number: {0}", AccountNumber);
+    }
+
+    public void Withdraw(double amount)
+    {
+        if (Balance >= amount)
+        {
+            Balance -= amount;
+            Console.WriteLine("Withdrawal successful! You withdrew: ${0}", amount);
+        }
+        else
+        {
+            Console.WriteLine("Insufficient balance. Cannot withdraw ${0}.", amount);
+        }
+    }
+
+    public void Deposit(double amount)
+    {
+        Balance += amount;
+        Console.WriteLine("Deposit successful! You deposited: ${0}", amount);
+    }
+
+    public void Check_And_Display_Balance()
+    {
+        Console.WriteLine("Current balance: ${0}", Balance);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        SavingAccount account1 = new SavingAccount("Anjali", 22421000, 1000.0);
+
+        account1.Display_Customer_Detail();
+        account1.Open_Account();
+
+        account1.Check_And_Display_Balance();
+
+        account1.Deposit(500.0);
+        account1.Check_And_Display_Balance();
+        account1.Withdraw(300.0);
+        account1.Check_And_Display_Balance();
+
+        account1.Close_Account();
+
+        Console.WriteLine("Bank Name: " + SavingAccount.BankName);
+    }
+}
